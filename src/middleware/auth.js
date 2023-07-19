4b6a7f274124a555232f7b092f4baaf05924800d
jaibersanchez <jaibersanchez.d@hotmail.com>
2023-07-18 22:26:38 -0500

    correccion segunda practica integradora

---
+++ src/middleware/auth.js
@@ -0,0 +1,14 @@
+
+export function isAdmin(req, res, next) {
+  if (req.session?.isAdmin) {
+    return next();
+  }
+  return res.status(403).render('error', { error: 'Error de autorización' });
+}
+
+export function isUser(req, res, next) {
+  if (req.session?.email) {
+    return next();
+  }
+  return res.status(401).render('error', { error: 'Error de autenticación' });
+}
